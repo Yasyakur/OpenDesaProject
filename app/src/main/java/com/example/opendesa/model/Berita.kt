edd55d46619522ae5b639cc87f46625a4a771c61
applepie25 <hanaulfia1612@gmail.com>
2023-01-08 16:09:27 +0700

    fix all beritadesa

--- app/src/main/java/com/example/opendesa/model/Berita.kt
+++ app/src/main/java/com/example/opendesa/model/Berita.kt
@@ -1,14 +1,35 @@
 package com.example.opendesa.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Berita(
+    @SerializedName("desa_id")
     val desa_id: String,
+
+    @SerializedName("nama_desa")
     val nama_desa: String,
+
+    @SerializedName("feed_link")
     val feed_link: String,
+
+    @SerializedName("link")
     val link: String,
+
+    @SerializedName("date")
     val date: String,
+
+    @SerializedName("author")
     val author: String,
+
+    @SerializedName("title")
     val title: String,
+
+    @SerializedName("image")
     val image: String,
+
+    @SerializedName("description")
     val description: String,
+
+    @SerializedName("content")
     val content: String
 )
